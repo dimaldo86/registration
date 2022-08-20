fc41b99fcf30810cf3f4c32080cd210b69f27950
Dmitry Menshikov <dimaldo86ne@mail.ru>
2022-08-21 02:57:11 +0700

    deploy

--- vue.config.js
+++ vue.config.js
@@ -4,20 +4,20 @@ module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
   ? '/registration/'
   : '/',
-  pages: {
-    index: {
-      // entry for the page
-      entry: 'src/main.js',
-      // the source template
-      template: 'public/index.html',
-      // output as dist/index.html
-      filename: 'index.html',
-      // when using title option,
-      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'Index Page',
-      // chunks to include on this page, by default includes
-      // extracted common chunks and vendor chunks.
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-    },
-  }
+  // pages: {
+  //   index: {
+  //     // entry for the page
+  //     entry: 'src/main.js',
+  //     // the source template
+  //     template: 'public/index.html',
+  //     // output as dist/index.html
+  //     filename: 'index.html',
+  //     // when using title option,
+  //     // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+  //     title: 'Index Page',
+  //     // chunks to include on this page, by default includes
+  //     // extracted common chunks and vendor chunks.
+  //     chunks: ['chunk-vendors', 'chunk-common', 'index']
+  //   },
+  // }
 })
